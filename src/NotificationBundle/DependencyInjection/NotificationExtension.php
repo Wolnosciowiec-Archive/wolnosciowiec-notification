1b1eb228ce4a516de6e8406dc4ec8b555129f6d3
Black and Red <wesoly.krzysztofa@gmail.com>
2017-01-29 19:28:40 +0100

    Adjustments and better logging

--- src/NotificationBundle/DependencyInjection/NotificationExtension.php
+++ src/NotificationBundle/DependencyInjection/NotificationExtension.php
@@ -4,7 +4,6 @@ namespace NotificationBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
